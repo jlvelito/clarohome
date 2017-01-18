8e29697b3f0453c1f1d9c5bb5f59fd5c45fa3bed
Luis Velito <hola@luisvelito.com>
2017-01-18 17:14:30 -0500

    First claro commit b

--- js/script.js
+++ js/script.js
@@ -61,5 +61,54 @@ $(document).ready(function(){
     }
   });
 
+  $('.usetype').on('click',function(){
+    $(this).toggleClass('active');
+  });
+
+  var calcFono = 0;
+  var calcLapt = 0;
+  var calcTvs = 0;
+  var calcConsol = 0;
+
+  $('#calc').on('click',function(){
+    $('#usarFonos ul li.active').each(function(){
+      var valor = parseFloat($(this).attr('data-use'));
+      var fonoValor = parseInt($('#telefonos').val());
+      calcFono = calcFono + valor;
+      $('#fonoCalc').val(calcFono*fonoValor);
+      console.log(calcFono*fonoValor);
+    });
+    $('#usarLaptop ul li.active').each(function(){
+      var valor = parseFloat($(this).attr('data-use'));
+      var fonoValor = parseInt($('#laptops').val());
+      calcLapt = calcLapt + valor;
+      $('#laptopCalc').val(calcLapt*fonoValor);
+      console.log(calcLapt*fonoValor);
+    });
+    $('#usarTv ul li.active').each(function(){
+      var valor = parseFloat($(this).attr('data-use'));
+      var fonoValor = parseInt($('#tv').val());
+      calcTvs = calcTvs + valor;
+      $('#tvCalc').val(calcTvs*fonoValor);
+      console.log(calcTvs*fonoValor);
+    });
+    $('#usarConsola ul li.active').each(function(){
+      var valor = parseFloat($(this).attr('data-use'));
+      var fonoValor = parseInt($('#consola').val());
+      calcConsol = calcConsol + valor;
+      $('#consolaCalc').val(calcConsol*fonoValor);
+      console.log(calcConsol*fonoValor);
+    });
+
+    setTimeout(function(){
+      var calcFono2 = parseFloat($('#fonoCalc').val());
+      var calcLapt2 = parseFloat($('#laptopCalc').val());
+      var calcTvs2 = parseFloat($('#tvCalc').val());
+      var calcConsol2 = parseFloat($('#consolaCalc').val());
+      $('#totalCalc').val(calcFono2+calcLapt2+calcTvs2+calcConsol2);
+     }, 300);
+
+  });
+
 
 });
